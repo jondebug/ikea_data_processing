5de09fa203a438d1f3247ec485e8de9b4785baed
jondebug <jonathanpaul202@gmail.com>
2022-12-28 19:52:53 +0200

    added fps translation script

---
+++ video_frame_rate_translation.py
@@ -0,0 +1,58 @@
+import json
+import os
+from utils import getListFromFile
+
+def createTranslationJson(all_timestamps, pv_timestamps, rec_dir):
+    trans = {}
+    video_frame_num = 0
+
+    for pv_frame_num, pv_timestamp in enumerate(pv_timestamps):
+        while video_frame_num<len(all_timestamps) and all_timestamps[video_frame_num] <= pv_timestamp:
+            trans[video_frame_num] = pv_frame_num
+            video_frame_num += 1
+
+    while video_frame_num < len(all_timestamps):
+        print("there are depth frames taken after last pv frame")
+        trans[video_frame_num] = len(pv_timestamps)-1
+        video_frame_num += 1
+    print(trans)
+    with open(os.path.join(rec_dir, "frame_rate_translation.json"), "w") as new_json_file_obj:
+        trans_json = json.dumps(trans)
+        new_json_file_obj.write(trans_json)
+        # return
+    return trans
+
+def createSingleFrameRateTranslation(rec_dir):
+    all_timestamps = []
+    pv_timestamps = []
+    assert os.path.exists(os.path.join(rec_dir, 'pv'))
+    for file in os.listdir(os.path.join(rec_dir, 'pv')):
+        filename = os.path.splitext(os.path.basename(file))[0]
+        all_timestamps.append(filename)
+        pv_timestamps.append(filename)
+
+    for file in os.listdir(os.path.join(rec_dir,  'Depth Long Throw')):
+        if (file.endswith(".pgm")  and not file.endswith("_ab.pgm")):
+            filename = os.path.splitext(os.path.basename(file))[0]
+            all_timestamps.append(filename)
+    all_timestamps.sort()
+    print(f"got {len(all_timestamps)} timestamps:\n all: {all_timestamps} \npv: {pv_timestamps}")
+    return createTranslationJson(all_timestamps, pv_timestamps, rec_dir)
+
+
+
+def createAllVideoFrameRateTranslation(dataset_top_dir):
+    indexing_files_path = os.path.join(dataset_top_dir, "indexing_files")
+    rec_dir_list_path = os.path.join(indexing_files_path, "all_recording_dir_list.txt")
+    rec_dir_list = getListFromFile(rec_dir_list_path)
+    for _dir_ in rec_dir_list:
+        all_timestamps, pv_timestamps = createSingleFrameRateTranslation(_dir_)
+
+
+
+
+
+if __name__ == "__main__":
+    dataset_dir = r'D:\test_recordings\Table_None_Paul_18092022_1216_recDir'
+    trans = createSingleFrameRateTranslation(dataset_dir)
+    print(trans[138])
